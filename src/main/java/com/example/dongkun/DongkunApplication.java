9bbc4eb1f687bd46868286e0d3954402082cc48f
nicky8209 <nicky8209@naver.com>
2023-04-25 17:52:03 +0900

    4. 책 입력 화면 만들기

---
+++ src/main/java/com/example/dongkun/DongkunApplication.java
@@ -0,0 +1,13 @@
+package com.example.dongkun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongkunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DongkunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dongkun/DongkunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dongkun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DongkunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
